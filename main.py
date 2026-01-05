3e43a8058d907f357c256a339d5f2d5a0b218973
AKo2425 <ko.atr@obmontessori.edu.ph>
2026-01-05 14:06:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Student Grade Calculator 
 
-from pyscript import document, display
+from pyscript import document
 
 subjects = ["Science", "Math", "English", "Filipino", "ICT", "PE"]
 units = (5, 5, 5, 3, 2, 1)
@@ -22,17 +22,21 @@ def calculate_gwa(e):
     weighted_sum = sum(grades[i] * units[i] for i in range(len(subjects)))
     gwa = weighted_sum / total_units
 
+    if gwa >= 75:
+        status = "Passed"
+    else:
+        status = "Failed"
     summary = f"""
     <h4>Name: {first_name} {last_name}</h4>
     <br>
-    {subjects[0]}: {grades[0]}<br>
-    {subjects[1]}: {grades[1]}<br>
-    {subjects[2]}: {grades[2]}<br>
-    {subjects[3]}: {grades[3]}<br>
-    {subjects[4]}: {grades[4]}<br>
-    {subjects[5]}: {grades[5]}<br><br>
-    <strong>Your general weighted average is: {gwa:.2f}</strong>
+    Science: {grades[0]}<br>
+    Math: {grades[1]}<br>
+    English: {grades[2]}<br>
+    Filipino: {grades[3]}<br>
+    ICT: {grades[4]}<br>
+    PE: {grades[5]}<br><br>
+    <strong>GWA: {gwa:.2f}</strong><br>
+    <strong>Status: {status}</strong>
     """
 
-    output_div = document.getElementById("output")
-    output_div.innerHTML = summary
+    document.getElementById("output").innerHTML = summary
